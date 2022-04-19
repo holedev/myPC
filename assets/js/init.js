6cb01dad9ad395158a27c3f76a14cb4e08827ac5
anvyidol <phanleho002@gmail.com>
2022-04-19 22:09:06 +0700

    update footer

---
+++ assets/js/init.js
@@ -0,0 +1,42 @@
+const weeks = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+const applicationsInit = [
+    {
+        id: 0,
+        icon: "./assets/img/apps/icon-this-pc.png",
+        name: "This PC",
+    },
+    {
+        id: 1,
+        icon: "./assets/img/apps/icon-recycle-bin.png",
+        name: "Recycle Bin",
+    },
+    {
+        id: 2,
+        icon: "./assets/img/apps/icon-garena.png",
+        name: "Garena",
+    },
+];
+
+export { weeks, months, applicationsInit };
