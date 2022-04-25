acaae09ef791524eff9d0bd66c848bab902be3e3
unknown <phanleho002@gmail.com>
2022-04-25 21:31:00 +0700

    update

--- assets/js/f-file.js
+++ assets/js/f-file.js
@@ -5,12 +5,31 @@ const $$ = document.querySelectorAll.bind(document);
     return {
         handle() {
             const icons = $$(".chap-8__content-head-icon");
+            const itemsTree = $$(".achievement-tree__item");
 
             icons.forEach((ic) => {
                 ic.onclick = function () {
                     this.parentElement.parentElement.classList.toggle("active");
                 };
             });
+
+            itemsTree.forEach((IT) => {
+                IT.onclick = function () {
+                    const title = this.querySelector("img").alt;
+                    const src = this.querySelector("img").src;
+
+                    const show = $(".f-file__achievement-show");
+
+                    show.querySelector("img").src = src;
+                    show.querySelector("span").innerHTML = title;
+
+                    show.classList.add("active");
+                };
+            });
+
+            $(".f-file__achievement-show .close-btn").onclick = function () {
+                $(".f-file__achievement-show").classList.remove("active");
+            };
         },
         start() {
             this.handle();
